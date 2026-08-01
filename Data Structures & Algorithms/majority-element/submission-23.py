ed5507c6b23a88cc429fd0f6c644faafa32538b2
tinyrah <139187362+tinyrah@users.noreply.github.com>
2026-08-01 12:07:26 -0500

    Add: majority-element - submission-23

---
+++ Data Structures & Algorithms/majority-element/submission-23.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        
+        for num, freq in count.items():
+            if freq >= len(nums) / 2:
+                return num
