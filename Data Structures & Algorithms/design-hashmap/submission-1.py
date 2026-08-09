b48b9b5f5d34686ef6517c241af110d30fe0f718
tinyrah <139187362+tinyrah@users.noreply.github.com>
2026-08-09 13:20:54 -0500

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.py
@@ -0,0 +1,54 @@
+class ListNode:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+class MyHashMap:
+
+    def __init__(self):
+        self.hashmap = [ListNode(0,0) for _ in range(1000)]
+
+    def put(self, key: int, value: int) -> None:
+        index = key % len(self.hashmap)
+        cur = self.hashmap[index]
+        
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.value = value
+                return
+            cur = cur.next
+        cur.next = ListNode(key, value)
+        
+
+    def get(self, key: int) -> int:
+        index = key % len(self.hashmap)
+        cur = self.hashmap[index]
+
+        while cur.next:
+            if cur.next.key == key:
+                return cur.next.value
+            cur = cur.next
+        return -1
+
+        
+
+    def remove(self, key: int) -> None:
+        index = key % len(self.hashmap)
+        cur = self.hashmap[index]
+
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
+
+        
+
+
+# Your MyHashMap object will b e instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
